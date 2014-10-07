722b9bdb4f08c113a937071d9091995e184259e2
Anthony Jaguenaud <capello@jaguenaud.org>
2014-10-07 23:14:03 +0200

    Correction

--- Premier.hs
+++ Premier.hs
@@ -3,7 +3,8 @@ prem' = 2:[a | a <- [3,5..], (product (map (\x -> mod a x) (takeWhile (<= trunca
 prem'' = 2:[a | a <- [3,5..], (all (/= 0) (map (\x -> mod a x) (takeWhile (<= truncate(sqrt (fromIntegral a::Float))) prem'))) ]
 
 prem2 = 2:[a | a <- [3,5..], (product (map (\x -> mod a x) ([3,5..isqrt a]))) /= 0 ]
-prem2' = 2:[a | a <- [3,5..], (product (map (\x -> mod a x) (takeWhile (<= isqrt a) prem'))) /= 0 ]
+prem2' = 2:[a | a <- [3,5..], (product (map (\x -> mod a x) (takeWhile (<= isqrt a) prem2'))) /= 0 ]
+prem2'' = 2:[a | a <- [3,5..], (all (/= 0) (map (\x -> mod a x) (takeWhile (<= isqrt a) prem2''))) ]
 
 table_des_carres :: [Int]
 table_des_carres = [a^2|a<-[1..]]
@@ -11,5 +12,5 @@ table_des_carres = [a^2|a<-[1..]]
 isqrt :: Int -> Int
 isqrt x = length (takeWhile (<= x) table_des_carres)
 
-main = putStrLn (show (prem !! 100000))
+main = putStrLn (show (prem2' !! 100000))
 
