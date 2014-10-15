0c176a3b9682507f2028fbc4919cdd2b8c5a085d
Anthony Jaguenaud <capello@jaguenaud.org>
2014-10-16 00:09:12 +0200

    Fin second degré

--- SecondDegre.hs
+++ SecondDegre.hs
@@ -1,10 +1,17 @@
-#!/usr/bin/runghc
+#!/usr/bin/runhaskell
+import System.Environment
 
 data Polynome2 = Polynome2 Float Float Float deriving (Show)
 
 data Solution2Degre = DeuxSolution { x1 :: Float, x2 :: Float } 
                      | UneSolution { x :: Float } 
                      | None deriving (Show)
+
+
+
+polyFromList :: [Float] -> Polynome2
+polyFromList (a:b:c:[]) = Polynome2 a b c
+polyFromList _ = error "Un polynome du second degré contient 3 parametres."
 
 resoudre :: Polynome2 -> Solution2Degre
 resoudre (Polynome2 a b c) 
@@ -13,5 +20,12 @@ resoudre (Polynome2 a b c)
       | otherwise = None
       where delta = b^2 - 4*a*c
 
-main = print $ resoudre (Polynome2 (-1) 1 30)
+main = do
+       args <- getArgs
+       if length args == 3
+       then do
+            let poly = map read args :: [Float]
+            print $ resoudre $ polyFromList poly
+       else
+           return ()
 
